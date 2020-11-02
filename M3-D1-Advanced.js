304de8b5ea329c930a224d6cb0ecc3b36eb73884
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-11-02 16:54:46 +0100

    Latest exercises 1-4

--- M3-D1-Advanced.js
+++ M3-D1-Advanced.js
@@ -184,7 +184,7 @@ Types of angles:
 
 */
 
-//TODO check
+//TODO check switch statement
 const findAngle = function (a) {
   let angle = a;
   console.log(angle);
@@ -279,6 +279,7 @@ Create a function to check from two given integers, whether one is positive and 
 
 */
 
+//TODO Check Math
 const checkPosOrNeg = function (x, y) {
   console.log(-Math.abs(x + y));
   if (Math.abs(x + y) > x + y && -Math.abs(x + y) !== x + y) {
@@ -379,3 +380,194 @@ const acroMaker = function (str) {
   return acronym.toUpperCase();
 };
 console.log(acroMaker("random access memory"));
+
+//--------------------------------Second File------------------------------------------
+/* 1) MAX CHAR
+Given a string, return the character that is most
+commonly used in the string.
+--- Examples
+    maxChar("abcccccccd") === "c"
+    maxChar("apple 1231111") === "1"
+*/
+
+const MaxChar = function (str) {
+  let count = {};
+  let value;
+  let key;
+  let arr = [...str];
+  for (let i = 0; i < arr.length; i++) {
+    key = arr[i];
+    value = count[key];
+    count[key] = value ? value + 1 : 1;
+  }
+  let arr_ = Object.values(count);
+  let max = Math.max(...arr_);
+
+  for (const c in count) {
+    if (count[c] === max) {
+      return c;
+    }
+  }
+};
+
+console.log(MaxChar("abbbbbbbbbbbd"));
+
+/* 2) ANAGRAMS
+Check to see if two provided strings are anagrams of each other.
+One string is an anagram of another if it uses the same characters
+in the same quantity. Only consider characters, not spaces
+or punctuation.  Consider capital letters to be the same as lower case
+--- Examples
+  anagrams('rail safety', 'fairy tales') --> True
+  anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+  anagrams('Hi there', 'Bye there') --> False
+*/
+
+const isAnagram = function (str1, str2) {
+  let map1 = {};
+  let map2 = {};
+  let key;
+  let value;
+  let arr1, arr2;
+  //str1.trim();
+  arr1 = [...str1.replace(/\s/g, "").toLowerCase()];
+  arr2 = [...str2.replace(/\s/g, "").toLowerCase()];
+  for (let i = 0; i < arr1.length; i++) {
+    key = arr1[i];
+    value = map1[key];
+    map1[key] = value ? value + 1 : 1;
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    key = arr2[i];
+    value = map2[key];
+    map2[key] = value ? value + 1 : 1;
+  }
+
+  let map1_properties = Object.getOwnPropertyNames(map1);
+  let map2_properties = Object.getOwnPropertyNames(map2);
+
+  if (map1_properties.length != map2_properties.length) {
+    return false;
+  }
+
+  for (let i = 0; i < map1_properties.length; i++) {
+    let property_name = map1_properties[i];
+    if (map1[property_name] !== map2[property_name]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isAnagram("Rail safy", "fairy taLes"));
+
+/* 3) ANAGRAMS 2
+Given a word and a list of possible anagrams, select the correct sublist.
+--- Examples 
+    "listen" and a list of candidates like "enlists" "google" "inlets" "banana" the program should return a list containing "inlets".
+*/
+
+const rightAnagram = function (str, candidates) {
+  for (let i = 0; i < candidates.length; i++) {
+    if (isAnagram(str, candidates[i])) {
+      return candidates[i];
+    }
+  }
+};
+
+console.log(rightAnagram("listen", ["enlists", "google", "inlets", "banana"]));
+
+/* 4) PALINDROME
+Given a string, return true if the string is a palindrome
+or false if it is not.  Palindromes are strings that
+form the same word if it is reversed. Do include spaces
+and punctuation in determining if the string is a palindrome.
+--- Examples:
+    palindrome("abba") === true
+    palindrome("abcdefg") === false
+ */
+
+/* 5) REVERSE INT
+Given an integer, return an integer that is the reverse
+ordering of numbers.
+--- Examples
+    reverseInt(15) === 51
+    reverseInt(981) === 189
+    reverseInt(500) === 5
+    reverseInt(-15) === -51
+    reverseInt(-90) === -9
+ */
+
+/* 6) STEPS
+Write a function that accepts a positive number N.
+The function should console log a step shape
+with N levels using the # character.  Make sure the
+step has spaces on the right hand side!
+--- Examples
+    steps(2)
+        '# '
+        '##'
+    steps(3)
+        '#  '
+        '## '
+        '###'
+    steps(4)
+        '#   '
+        '##  '
+        '### '
+        '####' */
+
+/* 7) REVERSE STRING
+Given a string, return a new string with the reversed
+order of characters
+--- Examples
+    reverse('apple') === 'leppa'
+    reverse('hello') === 'olleh'
+    reverse('Greetings!') === '!sgniteerG'
+ */
+
+/* 8) CHUNK
+Given an array and chunk size, divide the array into many subarrays
+where each subarray is of length size
+--- Examples
+    chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+    chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+    chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+    chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+    chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+*/
+
+/* 9) PYRAMID
+Write a function that accepts a positive number N.
+The function should console log a pyramid shape
+with N levels using the # character.  Make sure the
+pyramid has spaces on both the left and right hand sides
+--- Examples
+    pyramid(1)
+        '#'
+    pyramid(2)
+        ' # '
+        '###'
+    pyramid(3)
+        '  #  '
+        ' ### '
+        '#####' */
+
+/* 10) SPYRAL MATRIX
+Write a function that accepts an integer N
+and returns a NxN spiral matrix.
+--- Examples
+    matrix(2)
+        [[1, 2],
+        [4, 3]]
+    matrix(3)
+        [[1, 2, 3],
+        [8, 9, 4],
+        [7, 6, 5]]
+    matrix(4)
+        [[1, 2, 3, 4],
+        [12, 13, 14, 5],
+        [11, 16, 15, 6],
+        [10,  9,  8, 7]]
+*/
